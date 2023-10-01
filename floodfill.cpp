6ce170862d89527d471c40cc800123be71d4ce64
adarsh12344 <91307538+adarsh12344@users.noreply.github.com>
2023-10-02 00:51:15 +0530

    Add files via upload

---
+++ floodfill.cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+	int delcol[4] = {-1, 0, 1, 0};
+	int delrow[4] = {0, 1, 0, -1};
+	void dfs(int row, int col, vector<vector<int>> &ans, vector<vector<int>> &image, int newColor, int iniColor)
+	{
+		ans[row][col] = newColor; // changing color of current pixel
+		int n = image.size();	  // no of rows
+		int m = image[0].size();  // no of columns
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nrow = row + delrow[i];
+			int ncol = col + delcol[i];
+			if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) // nrow>=0 && nrow<n && ncol>=0 && ncol<m  --> checking if the pixel is inside the image
+																																// image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor   --> checking if the pixel is of same color as of current pixel
+																																// and it is not already colored with newColor
+			{
+				dfs(nrow, ncol, ans, image, newColor, iniColor);
+			}
+		}
+	}
+
+public:
+	vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor)
+	{
+		int iniColor = image[sr][sc];
+		vector<vector<int>> ans = image; // copying image to ans
+		dfs(sr, sc, ans, image, newColor, iniColor);
+		return ans;
+	}
+};
+
+int main()
+{
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>> image(n, vector<int>(m, 0));
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for (auto i : ans)
+		{
+			for (auto j : i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
